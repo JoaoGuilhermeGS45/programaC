ab92d5dc0afe75872d1eee1b4e4f0ac6ff61e791
JoaoGuilhermeGS45 <gamersparda45@gmail.com>
2025-03-04 19:55:25 +0000

    Prototipo do Super Trunfo

--- Prototipo do Super Trunfo.c
+++ Prototipo do Super Trunfo.c
@@ -52,34 +52,34 @@
         
         printf("\nCarta: 02\n");//Carta 02
 
-        printf("Digite a Letra do Estado:\n");
+        printf("Digite a Letra do Estado02:\n");
         scanf(" %c", &Estado);
 
-        printf("Digite o Codigo da Carta:\n");
+        printf("Digite o Codigo da Carta02:\n");
         scanf("%s", Codigo);
 
-        printf("Digite o Nome da Cidade:\n");
+        printf("Digite o Nome da Cidade02:\n");
         scanf("%s", Nome);
 
-        printf("Digite o Número da População:\n");
+        printf("Digite o Número da População02:\n");
         scanf("%d", &Populacao);
 
-        printf("Digite a Área da Cidade:\n");
+        printf("Digite a Área da Cidade02:\n");
         scanf("%f", &Area);
 
-        printf("Digite o PIB:\n");
+        printf("Digite o PIB02:\n");
         scanf("%f", &PIB);
 
-        printf("Digite o Número de Pontos Turísticos:\n");
+        printf("Digite o Número de Pontos Turísticos02:\n");
         scanf("%d", &Numero);
         
         printf("\nCarta: 02\n");
-        printf("Estado: %c\n",Estado);
-        printf("Código: %s\n",Codigo);
-        printf("Nome da Cidade: %s\n",Nome);
-        printf("População: %d\n",Populacao);
-        printf("Área: %.2f km²\n", Area);
-        printf("PIB: %.2f bilhões de reais\n", PIB);
-        printf("Número de Pontos Turísticos: %d\n", Numero);
+        printf("Estado02: %c\n",Estado);
+        printf("Código02: %s\n",Codigo);
+        printf("Nome da Cidade02: %s\n",Nome);
+        printf("População02: %d\n",Populacao);
+        printf("Área02: %.2f km²\n", Area);
+        printf("PIB02: %.2f bilhões de reais\n", PIB);
+        printf("Número de Pontos Turísticos02: %d\n", Numero);
         
     }
